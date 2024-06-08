baddcfaa5b2413b8092bf3b7ebabd199bb68e348
Jiraporn <jay.ji.jaroen@gmail.com>
2024-06-08 17:13:14 +0700

    edit .h

--- srcs/main.c
+++ srcs/main.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jjaroens <jjaroens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/03 12:59:35 by jjaroens          #+#    #+#             */
-/*   Updated: 2024/06/08 17:07:42 by jjaroens         ###   ########.fr       */
+/*   Created: 2024/06/08 16:58:05 by jjaroens          #+#    #+#             */
+/*   Updated: 2024/06/08 17:05:10 by jjaroens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO
-# define PHILO
-# include <pthread.h>
-# include <stdio.h>
-# include <stdlib.h>
+#include "../include/philo.h"
 
-typedef struct s_data
+// The program takes four arguments
+
+int main(int argc, char **argv)
 {
-	pthread_mutex_t	mutex;
-	
+	if (argc == 1 || argc < 5)
+	{
+		perror("Error: Check agrument\n");
+		exit (EXIT_FAILURE);
+	}
 }
-
-
-#endif
